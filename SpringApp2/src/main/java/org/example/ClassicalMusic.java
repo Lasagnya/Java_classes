4f25248784a111bd03aca0dc4248a312c0f065d2
Lasagnya <stepik-2003@yandex.ru>
2023-06-20 15:09:10 +0300

    create App2

---
+++ SpringApp2/src/main/java/org/example/ClassicalMusic.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class ClassicalMusic implements Music{
+	private int a;
+
+	@Override
+	public String getSong() {
+		return "Rhapsody";
+	}
+}
---
+++ SpringApp2/src/main/java/org/example/ShitMusic.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class ShitMusic implements Music{
+	@Override
+	public String getSong() {
+		return "Any rap";
+	}
+}
