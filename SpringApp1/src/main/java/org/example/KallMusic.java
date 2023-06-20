15e4aeb4a3c81bb9f51b5ddd9ce5566b8f7a69d4
Lasagnya <stepik-2003@yandex.ru>
2023-06-20 14:50:02 +0300

    started

---
+++ SpringApp1/src/main/java/org/example/KallMusic.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class KallMusic implements Music{
+	@Override
+	public String getSong() {
+		return "Any rap";
+	}
+}
---
+++ SpringApp1/src/main/java/org/example/NotMusic.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public interface NotMusic {
+	String getSong();
+}
