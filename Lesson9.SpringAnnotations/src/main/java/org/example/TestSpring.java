9a3cd691d536fb742159cd8f50ef9cf58c6599bf
Lasagnya <stepik-2003@yandex.ru>
2023-06-21 23:25:15 +0300

    Create lesson 9 and add Spring annotation Component.

---
+++ Lesson9.SpringAnnotations/src/main/java/org/example/TestSpring.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext.xml"
+		);
+		Music music = context.getBean("rockMusic", Music.class);
+		Music music2 = context.getBean("classicalMusic", Music.class);
+		MusicPlayer musicPlayer = new MusicPlayer(music);
+		MusicPlayer musicPlayer2 = new MusicPlayer(music2);
+		musicPlayer.playMusic();
+		musicPlayer2.playMusic();
+		context.close();
+	}
+}
