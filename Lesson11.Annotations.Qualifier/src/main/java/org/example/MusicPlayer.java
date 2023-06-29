aa4a5d09cfec5a0d5402c5276b891fb0f5c9b206
Lasagnya <stepik-2003@yandex.ru>
2023-06-29 23:18:19 +0300

    Add Qualifier in field.

--- Lesson11.Annotations.Qualifier/src/main/java/org/example/MusicPlayer.java
+++ Lesson11.Annotations.Qualifier/src/main/java/org/example/MusicPlayer.java
@@ -1,11 +1,13 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
 	@Autowired
+	@Qualifier("classicalMusic")
 	private Music music;
 
 	public String playMusic() {
