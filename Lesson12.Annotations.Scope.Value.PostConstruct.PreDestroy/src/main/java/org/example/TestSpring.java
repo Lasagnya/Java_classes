f934367064e0cf0ce0daeaacc673270cc9cbe6dd
Lasagnya <stepik-2003@yandex.ru>
2023-07-01 18:38:37 +0300

    Create.

---
+++ Lesson12.Annotations.Scope.Value.PostConstruct.PreDestroy/src/main/java/org/example/TestSpring.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext.xml"
+		);
+//		Music music = context.getBean("rockMusic", Music.class);
+//		Music music2 = context.getBean("classicalMusic", Music.class);
+//		MusicPlayer musicPlayer = new MusicPlayer(music);
+//		MusicPlayer musicPlayer2 = new MusicPlayer(music2);
+//		musicPlayer.playMusic();
+//		musicPlayer2.playMusic();
+
+//		MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//		musicPlayer.playMusic();
+
+		Computer computer = context.getBean("computer", Computer.class);
+		System.out.println(computer);
+		context.close();
+	}
+}
