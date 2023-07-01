740d9beed74e4f4c2bb6004cb0ff41788d712b21
Lasagnya <stepik-2003@yandex.ru>
2023-07-01 20:03:45 +0300

    Add PostConstruct, PreDestroy.

--- Lesson12.Annotations.Scope.Value.PostConstruct.PreDestroy/src/main/java/org/example/TestSpring.java
+++ Lesson12.Annotations.Scope.Value.PostConstruct.PreDestroy/src/main/java/org/example/TestSpring.java
@@ -7,23 +7,14 @@ public class TestSpring {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"applicationContext.xml"
 		);
-//		Music music = context.getBean("rockMusic", Music.class);
-//		Music music2 = context.getBean("classicalMusic", Music.class);
-//		MusicPlayer musicPlayer = new MusicPlayer(music);
-//		MusicPlayer musicPlayer2 = new MusicPlayer(music2);
-//		musicPlayer.playMusic();
-//		musicPlayer2.playMusic();
-
 //		MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//		musicPlayer.playMusic();
-
-		MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-		System.out.println(musicPlayer.getName());
-		System.out.println(musicPlayer.getVolume());
+//		System.out.println(musicPlayer.getName());
+//		System.out.println(musicPlayer.getVolume());
 
 		ClassicalMusic cm1 = context.getBean("classicalMusic", ClassicalMusic.class);
 		ClassicalMusic cm2 = context.getBean("classicalMusic", ClassicalMusic.class);
-		System.out.println(cm1 == cm2);
+		//System.out.println(cm1 == cm2);
+		System.out.println("line");
 		context.close();
 	}
 }
