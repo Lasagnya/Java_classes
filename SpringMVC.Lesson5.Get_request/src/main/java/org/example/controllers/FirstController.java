ab9f5d57017c3d2ff13ca8b3ce0c10c8d2b22624
Lasagnya <stepik-2003@yandex.ru>
2023-07-03 20:23:21 +0300

    Add @RequestParam.

--- SpringMVC.Lesson5.Get_request/src/main/java/org/example/controllers/FirstController.java
+++ SpringMVC.Lesson5.Get_request/src/main/java/org/example/controllers/FirstController.java
@@ -4,14 +4,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 	@GetMapping("/hello")
-	public String helloPage(HttpServletRequest request) {
-		String name = request.getParameter("name");
-		String surname = request.getParameter("surname");
+	public String helloPage(@RequestParam(value = "name") String name,		//обязательно ждёт параметры. Ошибка, если их нет.
+							@RequestParam(value = "surname", required = false) String surname) {//при таком параметре не обязательны, будут null лежать.
 		System.out.println(name + " " + surname);
 		return "first/hello";
 	}
