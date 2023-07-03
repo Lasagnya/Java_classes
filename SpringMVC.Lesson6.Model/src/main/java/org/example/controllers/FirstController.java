e996bedf8e6e5812d7967e8f8c8bcfcf7a256e10
Lasagnya <stepik-2003@yandex.ru>
2023-07-03 20:56:56 +0300

    Add Model.

--- SpringMVC.Lesson6.Model/src/main/java/org/example/controllers/FirstController.java
+++ SpringMVC.Lesson6.Model/src/main/java/org/example/controllers/FirstController.java
@@ -5,14 +5,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 	@GetMapping("/hello")
-	public String helloPage(@RequestParam(value = "name") String name,		//обязательно ждёт параметры. Ошибка, если их нет.
-							@RequestParam(value = "surname", required = false) String surname) {//при таком параметре не обязательны, будут null лежать.
-		System.out.println(name + " " + surname);
+	public String helloPage(@RequestParam(value = "name") String name,							//обязательно ждёт параметры. Ошибка, если их нет.
+							@RequestParam(value = "surname", required = false) String surname,	//при таком параметре не обязательны, будут null лежать.
+							Model model) {
+		model.addAttribute("message", name + " " + surname);
 		return "first/hello";
 	}
 
