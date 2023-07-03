221fea480b3455b40e98e5cfcf1ab8ba27a4e7a5
Lasagnya <stepik-2003@yandex.ru>
2023-07-03 22:17:45 +0300

    Create.

---
+++ SpringMVC.Lesson6.Homework_calculator/src/main/java/org/example/controllers/FirstController.java
@@ -0,0 +1,24 @@
+package org.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+	@GetMapping("/hello")
+	public String helloPage(@RequestParam(value = "name") String name,							//обязательно ждёт параметры. Ошибка, если их нет.
+							@RequestParam(value = "surname", required = false) String surname,	//при таком параметре не обязательны, будут null лежать.
+							Model model) {
+		model.addAttribute("message", name + " " + surname);
+		return "first/hello";
+	}
+
+	@GetMapping("/goodbye")
+	public String goodbyePage() {
+		return "first/goodbye";
+	}
+}
