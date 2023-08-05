0bd968b80a92dfe4550f8e29f689ccdce033995b
Lasagnya <stepik-2003@yandex.ru>
2023-08-05 13:42:08 +0300

    Create and add cascading.

---
+++ Hibernate.Lesson6.Cascading/src/main/java/org/example/App.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.example.model.Item;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.example.model.Person;
+import org.hibernate.query.Query;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            session.beginTransaction();
+
+            Person person = new Person("Test cascading", 30);
+            Item item = new Item("Test cascading item", person);
+            person.setItems(new ArrayList<>(Collections.singletonList(item)));
+            session.persist(person);
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
