3d03cbeb7225fa7f524cd5d0cd75b43a27d72693
Lasagnya <stepik-2003@yandex.ru>
2023-08-07 16:03:25 +0300

    Add all.

--- Hibernate.Homework1/src/main/java/org/example/App.java
+++ Hibernate.Homework1/src/main/java/org/example/App.java
@@ -1,24 +1,51 @@
 package org.example;
 
+import org.example.model.Director;
+import org.example.model.Movie;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.util.Collections;
+import java.util.Deque;
+import java.util.List;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Director.class).addAnnotatedClass(Movie.class);
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
         try {
             session.beginTransaction();
 
-            Person person = new Person("Test cascading", 30);
-            person.addItem(new Item("Item1"));
-            person.addItem(new Item("Item2"));
-            person.addItem(new Item("Item3"));
-            session.persist(person);
+//            Director director1 = session.get(Director.class, 3);
+//            List<Movie> movies1 = director1.getMovies();
+
+//            Movie movie2 = session.get(Movie.class, 5);
+//            Director director2 = movie2.getDirector();
+
+//            Director director3 = session.get(Director.class, 5);
+//            Movie movie3 = new Movie("Test", 2000, director3);
+//            session.persist(movie3);
+//            director3.getMovies().add(movie3);
+
+//            Director director4 = new Director("Anonym", 15);
+//            Movie movie4 = new Movie("Anonym's film", 2023, director4);
+//            director4.setMovies(Collections.singletonList(movie4));
+//            session.persist(director4);
+
+//            Movie movie5 = session.get(Movie.class, 12);
+//            Director director5 = session.get(Director.class, 4);
+//            movie5.getDirector().getMovies().remove(movie5);
+//            movie5.setDirector(director5);
+//            director5.getMovies().add(movie5);
+
+            Movie movie6 = session.get(Movie.class, 12);
+            Director director6 = movie6.getDirector();
+            director6.getMovies().remove(movie6);
+            session.remove(movie6);
 
             session.getTransaction().commit();
         } finally {
--- Hibernate.Homework1/src/main/java/org/example/model/Movie.java
+++ Hibernate.Homework1/src/main/java/org/example/model/Movie.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "movie")
 public class Movie {
 	@Id
-	@Column(name = "id")
+	@Column(name = "movie_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
@@ -17,6 +17,47 @@ public class Movie {
 	private int year_of_production;
 
 	@ManyToOne
-	@JoinColumn(name = "director_id", referencedColumnName = "id")
+	@JoinColumn(name = "director_id", referencedColumnName = "director_id")
 	private Director director;
+
+	public Movie() {
+	}
+
+	public Movie(String name, int year_of_production, Director director) {
+		this.name = name;
+		this.year_of_production = year_of_production;
+		this.director = director;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getYear_of_production() {
+		return year_of_production;
+	}
+
+	public void setYear_of_production(int year_of_production) {
+		this.year_of_production = year_of_production;
+	}
+
+	public Director getDirector() {
+		return director;
+	}
+
+	public void setDirector(Director director) {
+		this.director = director;
+	}
 }
