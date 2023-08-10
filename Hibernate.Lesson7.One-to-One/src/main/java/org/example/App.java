4d9f254b044858f80850d1c5751562e17a5768d6
Lasagnya <stepik-2003@yandex.ru>
2023-08-10 17:19:57 +0300

    Create and add OneToOne create.

---
+++ Hibernate.Lesson7.One-to-One/src/main/java/org/example/App.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.example.model.Passport;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.example.model.Person;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Passport.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        try {
+            session.beginTransaction();
+
+            Person person = new Person("Test person", 65);
+            Passport passport = new Passport(person, 12345);
+            person.setPassport(passport);
+            session.persist(person);
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
