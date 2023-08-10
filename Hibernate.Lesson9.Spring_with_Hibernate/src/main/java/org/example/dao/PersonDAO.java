47a6deb3070900454c88844654bb05ade547b44a
Lasagnya <stepik-2003@yandex.ru>
2023-08-11 02:45:14 +0300

    Add hibernate in index.

--- Hibernate.Lesson9.Spring_with_Hibernate/src/main/java/org/example/dao/PersonDAO.java
+++ Hibernate.Lesson9.Spring_with_Hibernate/src/main/java/org/example/dao/PersonDAO.java
@@ -1,48 +1,43 @@
 package org.example.dao;
 
 import org.example.models.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.*;
 import java.util.List;
 
 @Component
 public class PersonDAO {
-	private final JdbcTemplate jdbcTemplate;
-
-	private static int PEOPLE_COUNT = 0;
+	private final SessionFactory sessionFactory;
 
 	@Autowired
-	public PersonDAO(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
+	public PersonDAO(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
 	}
 
-	public List<Person> index(){
-		//return jdbcTemplate.query("select * from person", new PersonMapper());		//с собственным RowMapper
-		return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<>(Person.class));
+	@Transactional(readOnly = true)
+	public List<Person> index() {
+		Session session = sessionFactory.getCurrentSession();
+		List<Person> people = session.createQuery("select p from Person p", Person.class).getResultList();
+		return people;
 	}
 
 	public Person show(int id) {
-		return jdbcTemplate.query("select * from person where id=?", new Object[]{id}, new int[]{Types.INTEGER}, new BeanPropertyRowMapper<>(Person.class))
-				.stream().findAny().orElse(null);
+		return null;
 	}
 
 	public void save(Person person) {
-		int[] argTypes = new int[]{Types.INTEGER, Types.VARCHAR, Types.INTEGER, Types.VARCHAR};
-		jdbcTemplate.update("insert into person values(?, ?, ?, ?)",
-				new Object[]{++PEOPLE_COUNT, person.getName(), person.getAge(), person.getEmail()}, argTypes);
+
 	}
 
 	public void update(int id, Person updatedPerson) {
-		int[] argTypes = new int[]{Types.VARCHAR, Types.INTEGER, Types.VARCHAR, Types.INTEGER};
-		jdbcTemplate.update("update person set name=?, age=?, email=? where id=?",
-				new Object[]{updatedPerson.getName(), updatedPerson.getAge(), updatedPerson.getEmail(), id}, argTypes);
+
 	}
 
 	public void delete(int id) {
-		jdbcTemplate.update("delete from person where id = ?", new Object[]{id}, new int[]{Types.INTEGER});
+
 	}
 }
--- Hibernate.Lesson9.Spring_with_Hibernate/src/main/java/org/example/models/Person.java
+++ Hibernate.Lesson9.Spring_with_Hibernate/src/main/java/org/example/models/Person.java
@@ -1,30 +1,32 @@
 package org.example.models;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 
+@Entity
+@Table(name = "person")
 public class Person {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private int id;
 
 	@NotEmpty(message = "Name should not be empty")
 	@Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+	@Column(name = "name")
 	private String name;
 
 	@Min(value = 0, message = "Age should be greater than 0")
 	@Max(value = 150, message = "Age should be less than 150")
+	@Column(name = "age")
 	private int age;
-
-	@NotEmpty(message = "Email should not be empty")
-	@Email(message = "Email should be valid")
-	private String email;
 
 	public Person() {
 	}
 
-	public Person(int id, String name, int age, String email) {
-		this.id = id;
+	public Person(String name, int age) {
 		this.name = name;
 		this.age = age;
-		this.email = email;
 	}
 
 	public int getId() {
@@ -49,13 +51,5 @@ public class Person {
 
 	public void setAge(int age) {
 		this.age = age;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
 	}
 }
