14903b8d7c81dce8c44c0ef56959d65e587d73ab
Lasagnya <stepik-2003@yandex.ru>
2023-08-16 16:54:59 +0300

    Create.

---
+++ Hibernate.Lesson12.N_plus_1_problem/src/main/java/org/example/models/Item.java
@@ -0,0 +1,53 @@
+package org.example.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "item")
+public class Item {
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@NotEmpty
+	@Column(name="item_name")
+	private String name;
+
+	@ManyToOne
+	@JoinColumn(name = "person_id", referencedColumnName = "id")
+	private Person owner;
+
+	public Item() {
+	}
+
+	public Item(String name, Person owner) {
+		this.name = name;
+		this.owner = owner;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Person getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Person owner) {
+		this.owner = owner;
+	}
+}
